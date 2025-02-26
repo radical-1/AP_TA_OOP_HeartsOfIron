43f1d933066c9cf5088e2a3c467c61c2678ebf11
Sana Niroomand <sana.niroomand@gmail.com>
2025-02-26 21:55:12 +0330

    improving terrain and weather classes

--- src/model/Terrain.java
+++ src/model/Terrain.java
@@ -1,5 +1,19 @@
 package model;
 
 public enum Terrain {
+    MOUNTAIN,
+    FOREST,
+    DESERT,
+    URBAN,
+    PLAIN;
 
+    public void modify(Battalion battalion) {
+        switch (this) {
+            case MOUNTAIN: break;
+            case FOREST: break;
+            case DESERT: break;
+            case URBAN: break;
+            case PLAIN: break;
+        }
+    }
 }
