bb8072b4548516efa7c0b6566b597ee6805e7d64
Sana Niroomand <sana.niroomand@gmail.com>
2025-02-26 21:24:20 +0330

    corrected and completed the neighbors list in the Tile enum

--- src/model/Tile.java
+++ src/model/Tile.java
@@ -2,6 +2,7 @@ package model;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public enum Tile {
     TILE1(1, Country.SOVIET_UNION),
@@ -90,47 +91,56 @@ public enum Tile {
         TILE13.landNeighbors.addAll(Arrays.asList(TILE9, TILE14));
         TILE14.landNeighbors.addAll(Arrays.asList(TILE10, TILE13, TILE15));
         TILE15.landNeighbors.addAll(Arrays.asList(TILE11, TILE14, TILE16));
-        TILE16.landNeighbors.addAll(Arrays.asList(TILE12, TILE15, TILE39));
-        TILE17.landNeighbors.addAll(Arrays.asList(TILE18, TILE23));
-        TILE18.landNeighbors.addAll(Arrays.asList(TILE17, TILE19, TILE24));
-        TILE19.landNeighbors.addAll(Arrays.asList(TILE18, TILE20, TILE25));
-        TILE20.landNeighbors.addAll(Arrays.asList(TILE19, TILE21, TILE26));
-        TILE21.landNeighbors.addAll(Arrays.asList(TILE20, TILE22, TILE27));
-        TILE22.landNeighbors.addAll(Arrays.asList(TILE21, TILE28));
+        TILE16.landNeighbors.addAll(Arrays.asList(TILE12, TILE15, TILE21, TILE22, TILE39));
+        TILE17.landNeighbors.addAll(Arrays.asList(TILE14, TILE18, TILE23));
+        TILE18.landNeighbors.addAll(Arrays.asList(TILE14, TILE17, TILE19, TILE24));
+        TILE19.landNeighbors.addAll(Arrays.asList(TILE15, TILE18, TILE20, TILE25));
+        TILE20.landNeighbors.addAll(Arrays.asList(TILE15, TILE19, TILE21, TILE26));
+        TILE21.landNeighbors.addAll(Arrays.asList(TILE16, TILE20, TILE22, TILE27));
+        TILE22.landNeighbors.addAll(Arrays.asList(TILE16, TILE21, TILE28, TILE43));
         TILE23.landNeighbors.addAll(Arrays.asList(TILE17, TILE24, TILE29));
         TILE24.landNeighbors.addAll(Arrays.asList(TILE18, TILE23, TILE25, TILE30));
         TILE25.landNeighbors.addAll(Arrays.asList(TILE19, TILE24, TILE26, TILE31));
         TILE26.landNeighbors.addAll(Arrays.asList(TILE20, TILE25, TILE27, TILE32));
         TILE27.landNeighbors.addAll(Arrays.asList(TILE21, TILE26, TILE28, TILE33));
-        TILE28.landNeighbors.addAll(Arrays.asList(TILE22, TILE27, TILE34));
+        TILE28.landNeighbors.addAll(Arrays.asList(TILE22, TILE27, TILE34, TILE47));
         TILE29.landNeighbors.addAll(Arrays.asList(TILE23, TILE30));
         TILE30.landNeighbors.addAll(Arrays.asList(TILE24, TILE29, TILE31));
         TILE31.landNeighbors.addAll(Arrays.asList(TILE25, TILE30, TILE32));
         TILE32.landNeighbors.addAll(Arrays.asList(TILE26, TILE31, TILE33));
         TILE33.landNeighbors.addAll(Arrays.asList(TILE27, TILE32, TILE34));
         TILE34.landNeighbors.addAll(Arrays.asList(TILE28, TILE33));
-        TILE35.landNeighbors.addAll(Arrays.asList(TILE36, TILE39));
+        TILE35.landNeighbors.addAll(Arrays.asList(TILE25, TILE36, TILE39));
         TILE36.landNeighbors.addAll(Arrays.asList(TILE35, TILE37, TILE40));
-        TILE37.landNeighbors.addAll(Arrays.asList(TILE36, TILE38, TILE41));
-        TILE38.landNeighbors.addAll(Arrays.asList(TILE37, TILE42));
+        TILE37.landNeighbors.addAll(Arrays.asList(TILE54, TILE36, TILE38, TILE41));
+        TILE38.landNeighbors.addAll(Arrays.asList(TILE37, TILE42, TILE55));
         TILE39.landNeighbors.addAll(Arrays.asList(TILE16, TILE35, TILE40, TILE43));
         TILE40.landNeighbors.addAll(Arrays.asList(TILE36, TILE39, TILE41, TILE44));
         TILE41.landNeighbors.addAll(Arrays.asList(TILE37, TILE40, TILE42, TILE45));
         TILE42.landNeighbors.addAll(Arrays.asList(TILE38, TILE41, TILE46));
-        TILE43.landNeighbors.addAll(Arrays.asList(TILE39, TILE44, TILE47));
+        TILE43.landNeighbors.addAll(Arrays.asList(TILE22, TILE39, TILE44, TILE47));
         TILE44.landNeighbors.addAll(Arrays.asList(TILE40, TILE43, TILE45, TILE48));
         TILE45.landNeighbors.addAll(Arrays.asList(TILE41, TILE44, TILE46, TILE49));
         TILE46.landNeighbors.addAll(Arrays.asList(TILE42, TILE45, TILE50));
-        TILE47.landNeighbors.addAll(Arrays.asList(TILE43, TILE48));
+        TILE47.landNeighbors.addAll(Arrays.asList(TILE28, TILE43, TILE48));
         TILE48.landNeighbors.addAll(Arrays.asList(TILE44, TILE47, TILE49));
         TILE49.landNeighbors.addAll(Arrays.asList(TILE45, TILE48, TILE50));
-        TILE50.landNeighbors.addAll(Arrays.asList(TILE46, TILE49));
-        TILE51.landNeighbors.addAll(Arrays.asList(TILE52, TILE53));
-        TILE52.landNeighbors.addAll(Arrays.asList(TILE51, TILE53));
+        TILE50.landNeighbors.addAll(Arrays.asList(TILE46, TILE49, TILE51, TILE52));
+        TILE51.landNeighbors.addAll(Arrays.asList(TILE50, TILE53));
+        TILE52.landNeighbors.addAll(Arrays.asList(TILE50, TILE53));
         TILE53.landNeighbors.addAll(Arrays.asList(TILE51, TILE52));
-        TILE54.landNeighbors.addAll(Arrays.asList(TILE55, TILE56));
-        TILE55.landNeighbors.addAll(Arrays.asList(TILE54, TILE56));
-        TILE56.landNeighbors.addAll(Arrays.asList(TILE54, TILE55));
+        TILE54.landNeighbors.addAll(Arrays.asList(TILE37, TILE55));
+        TILE55.landNeighbors.addAll(Arrays.asList(TILE38, TILE54, TILE56));
+        TILE56.landNeighbors.add(TILE55);
+        TILE8.maritimeNeighbors.addAll(Arrays.asList(TILE35, TILE36, TILE54));
+        TILE35.maritimeNeighbors.addAll(Arrays.asList(TILE8, TILE36, TILE54));
+        TILE36.maritimeNeighbors.addAll(Arrays.asList(TILE8, TILE35, TILE54));
+        TILE54.maritimeNeighbors.addAll(Arrays.asList(TILE8, TILE35, TILE36));
+        TILE34.maritimeNeighbors.addAll(Arrays.asList(TILE47, TILE48, TILE49, TILE52));
+        TILE47.maritimeNeighbors.addAll(Arrays.asList(TILE34, TILE48, TILE49, TILE52));
+        TILE48.maritimeNeighbors.addAll(Arrays.asList(TILE34, TILE47, TILE49, TILE52));
+        TILE49.maritimeNeighbors.addAll(Arrays.asList(TILE34, TILE47, TILE48, TILE52));
+        TILE52.maritimeNeighbors.addAll(Arrays.asList(TILE34, TILE47, TILE48, TILE49));
     }
 
     public int getIndex() {
