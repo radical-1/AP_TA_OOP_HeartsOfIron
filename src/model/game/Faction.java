da942343a9f73ebf5e92779339da433bdf59991b
Sana Niroomand <sana.niroomand@gmail.com>
2025-03-06 01:11:04 +0330

    fixed some bad mismerging

--- src/model/game/Faction.java
+++ src/model/game/Faction.java
@@ -1,4 +1,23 @@
 package model.game;
 
+import java.util.ArrayList;
+
 public record Faction(String name) {
+    private static final ArrayList<Faction> factions;
+
+    static {
+        factions = new ArrayList<>();
+    }
+
+    public Faction(String name) {
+        this.name = name;
+        factions.add(this);
+    }
+
+    public static Faction getFactionByName(String name) {
+        for (Faction faction : factions) {
+            if (faction.name.equals(name)) return faction;
+        }
+        return null;
+    }
 }
